9ba41f0531b66197678553743b212c2452f80a7b
davidPetrosyanDev <david.petrosyandev@gmail.com>
2018-06-22 20:08:14 -0400

    initial ejs and css setup

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 var express = require('express')
+var path = require('path')
 var app = express()
 
+app.set('view engine', "ejs")
+app.use(express.static(__dirname, +"/build/public"))
+app.set('views', path.join(__dirname, '/src/views'));
+
+app.get("/", function(req, res){
+    res.render("index")
+})
+
+app.listen(3000, function(){
+    console.log("Server running on port 3000")
+})
---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+const gulp = require("gulp")
+const imagemin = require("gulp-imagemin")
+const uglify = require('gulp-uglify')
+const sass = require('gulp-sass')
+const browserSync = require('browser-sync').create();
+const nodemon = require('gulp-nodemon')
+
+//Copy all ejs files
+gulp.task('copyEjs', function () {
+    gulp.src("src/views/*.ejs")
+        .pipe(gulp.dest("build/views"))
+        .pipe(browserSync.stream());
+})
+
+//Optimize images
+// gulp.task('imageMin', () =>
+//     gulp.src('src/images/*')
+//         .pipe(imagemin())
+//         .pipe(gulp.dest('build/public/images'))
+// );
+
+//Minify js
+gulp.task('copyJs', function(){
+    gulp.src('src/js/*.js')
+        // .pipe(uglify())
+        .pipe(gulp.dest('build/public/js'))
+        .pipe(browserSync.stream());
+})
+
+//Compile SASS 
+gulp.task('compile-sass', function(){
+    gulp.src('src/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('build/public/css'))
+        .pipe(browserSync.stream());
+})
+
+gulp.task('watch', function(){
+    gulp.watch('src/sass/**/*.scss',['compile-sass'])
+    gulp.watch('src/js/*.js',['copyJs'])
+    gulp.watch('src/views/*.ejs',['copyEjs'])
+})
+
+gulp.task('browser-sync', function () {
+    browserSync.init(null, {
+        proxy: "http://localhost:3000", // port of node server
+    });
+});
+
+gulp.task('default', ['copyEjs','copyJs','compile-sass','browser-sync','watch'], function () {
+});
