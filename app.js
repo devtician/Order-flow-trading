8ba088f6a573badd471e65c556ebca4e4fdd2a97
devtician <david.petrosyandev@gmail.com>
2018-09-06 15:15:02 -0400

    add ping timeout and interval

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, { 'pingTimeout': 7000, 'pingInterval': 1000 });
 const cors = require('cors');
 
 // CONFIG
